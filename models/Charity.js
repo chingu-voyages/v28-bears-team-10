b629b1229d52aaa65a80a4d52a15890eee93cac2
Armando Guarino <arm.guarino@gmail.com>
2021-03-11 15:58:48 +0100

    Add schemas

--- models/Charity.js
+++ models/Charity.js
@@ -6,6 +6,37 @@ var charity = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    skills: {
+        type: Array
+    },
+    description: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    job_listings: {
+        type: Array
+    },
+    website: {
+        type: String
+    },
+    twitter: {
+        type: String
+    },
+    linkedin: {
+        type: String
+    },
+    github: {
+        type: String
+    }
 });
 
 mongoose.models = {};
--- models/Dev.js
+++ models/Dev.js
@@ -2,10 +2,53 @@ import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
 var dev = new Schema({
-    name: {
+    username: {
         type: String,
         required: true
     },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    skills: {
+        type: Array
+    },
+    description: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    twitter: {
+        type: String
+    },
+    linkedin: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 mongoose.models = {};
