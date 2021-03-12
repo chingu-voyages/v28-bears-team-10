3507d6f157535f5e651ceee51c83a297fc1f7f77
Armando Guarino <arm.guarino@gmail.com>
2021-03-12 13:00:35 +0100

    Add charities routes

--- models/Charity.js
+++ models/Charity.js
@@ -1,46 +1,46 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 var Schema = mongoose.Schema;
 
 var charity = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-    },
-    skills: {
-        type: Array
-    },
-    description: {
-        type: String
-    },
-    avatar: {
-        type: String
-    },
-    job_listings: {
-        type: Array
-    },
-    website: {
-        type: String
-    },
-    twitter: {
-        type: String
-    },
-    linkedin: {
-        type: String
-    },
-    github: {
-        type: String
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+  skills: {
+    type: Array,
+  },
+  description: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  job_listings: {
+    type: Array,
+  },
+  website: {
+    type: String,
+  },
+  twitter: {
+    type: String,
+  },
+  linkedin: {
+    type: String,
+  },
+  github: {
+    type: String,
+  },
 });
 
 mongoose.models = {};
 
-var Charity = mongoose.model('Charity', charity);
+var Charity = mongoose.model("Charity", charity);
 
 export default Charity;
--- pages/api/charities/index.js
+++ pages/api/charities/index.js
@@ -1,67 +1,57 @@
 import connectDB from "../../../middleware/db";
 
-import Dev from "../../../models/dev";
+import Charity from "../../../models/charity";
 
-const devs = async (req, res) => {
-  // @route     GET api/devs
-  // @desc      Get all devs
+const charities = async (req, res) => {
+  // @route     GET api/charities
+  // @desc      Get all charities
   // @access    Public
   if (req.method === "GET") {
     try {
       // Get all devs in database
-      const devs = await Dev.find();
+      const charities = await Charity.find();
 
-      res.status(200).json(devs);
+      res.status(200).json(charities);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
     }
 
-    // @route     POST api/devs
+    // @route     POST api/charities
     // @desc      Add new dev
     // @access    Public
   } else if (req.method === "POST") {
     // @todo add form data validation
     // Destructure data coming from form
     const {
-      username,
-      first_name,
-      last_name,
+      name,
       email,
-      gender,
       location,
-      skills,
       description,
       avatar,
+      job_listings,
       website,
       twitter,
-      linkedin,
-      github,
     } = req.body;
 
     try {
-      // Create new dev object with fields coming from form
-      const newDev = new Dev({
-        username,
-        first_name,
-        last_name,
+      // Create new dev object with fields coming from db
+      const newCharity = new Charity({
+        name,
         email,
-        gender,
         location,
-        skills,
         description,
         avatar,
+        job_listings,
         website,
         twitter,
-        linkedin,
-        github,
       });
 
       // Save dev to db
-      const dev = await newDev.save();
+      const charity = await newCharity.save();
 
       // Return new dev
-      res.status(200).json(dev);
+      res.status(200).json(charity);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
@@ -69,4 +59,4 @@ const devs = async (req, res) => {
   }
 };
 
-export default connectDB(devs);
+export default connectDB(charities);
