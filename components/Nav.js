f6c2a9a10be7eb22abae73e50fb45e1d2a55abe8
Jon <56552404+jondhill333@users.noreply.github.com>
2021-03-20 12:27:17 +0200

    Feature/navbar (#48)
    * Update indents from styles ocnfig
    
    * Add layout and nav basic components across all pages
    
    * Add styles to nav and update wording from home page
    
    * Add chakra components and implement into nav with buttons for sign up

---
+++ components/Nav.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Box, Center, Container, Flex, Text, Spacer } from '@chakra-ui/layout';
+import { Tooltip } from '@chakra-ui/react';
+import { InfoOutlineIcon } from '@chakra-ui/icons';
+
+export default function Nav({ children }) {
+    return (
+        <>
+            <Flex
+                borderBottom=" 2px solid orange"
+                pos="fixed"
+                top={0}
+                w="100vw"
+                height="70px"
+                direction="row"
+                align="center"
+                justify="space-between"
+                color="black"
+                px="2%">
+                <Box>
+                    <Text>Logo Container</Text>
+                </Box>
+
+                <Spacer />
+                {/* <Spacer /> */}
+                <Tooltip label="Coming Soon" aria-label="A tooltip">
+                    <Box mx="5px">
+                        <InfoOutlineIcon w={6} h={6} />
+                    </Box>
+                </Tooltip>
+
+                <Tooltip label="Coming Soon" aria-label="A tooltip">
+                    <Box
+                        mx="5px"
+                        as="button"
+                        borderRadius="md"
+                        bg="blue"
+                        color="white"
+                        px={2}
+                        h={10}>
+                        <Text p="0" fontWeight="600">
+                            CHARITY SIGN UP
+                        </Text>
+                    </Box>
+                </Tooltip>
+                <Tooltip label="Coming Soon" aria-label="A tooltip">
+                    <Box
+                        mx="5px"
+                        as="button"
+                        borderRadius="md"
+                        bg="orange"
+                        color="white"
+                        px={2}
+                        h={10}>
+                        <Text fontWeight="600">VOLUNTEER SIGN UP</Text>
+                    </Box>
+                </Tooltip>
+            </Flex>
+        </>
+    );
+}
