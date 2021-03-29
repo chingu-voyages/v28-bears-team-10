4424a87fafdff93ea81e1677dba1794da6d77618
Greg Ogun <gregogun97@gmail.com>
2021-03-29 08:06:30 +0100

    style: add Chakra extend theme
    add extend theme for customisation of global CSS rules based on design decisions

--- styles/theme.js
+++ styles/theme.js
@@ -1,9 +1,22 @@
-export default {
-  colors: {
-    transparent: "transparent",
-    black: "#000",
-    white: "#fff",
-    orange: "#FF6700",
-    blue: "#268ED0",
-  },
-};
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    styles: {
+        global: (props) => ({
+            body: {
+                overflowX: 'hidden',
+                color: props.colorMode === 'light' ? 'white' : 'black',
+                bg: props.colorMode === 'light' ? 'white' : 'black'
+            }
+        })
+    },
+    colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        orange: '#FF6700',
+        blue: '#268ED0'
+    }
+});
+
+export default theme;
