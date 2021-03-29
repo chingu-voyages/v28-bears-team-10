d562130f9c22186bb38402201d7eb31099921b86
Greg Ogun <gregogun97@gmail.com>
2021-03-29 09:48:42 +0100

    refactor: typo fixes

--- styles/font-face.js
+++ styles/font-face.js
@@ -4,9 +4,9 @@ export default `
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: url(public/fonts/GlacialIndifference-Regular.woff2) format('woff2');
-      url(public/fonts/GlacialIndifference-Regular.woff) format('woff');
-      url(public/fonts/GlacialIndifference-Regular.eot) format('eot');
+      src: url(fonts/GlacialIndifference-Regular.woff2) format('woff2');
+      url(fonts/GlacialIndifference-Regular.woff) format('woff');
+      url(fonts/GlacialIndifference-Regular.eot) format('eot');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
     @font-face {
@@ -14,9 +14,9 @@ export default `
       font-style: bold;
       font-weight: 700;
       font-display: swap;
-      src: url(public/fonts/GlacialIndifference-Bold.woff2) format('woff2');
-      url(public/fonts/GlacialIndifference-Bold.woff) format('woff');
-      url(public/fonts/GlacialIndifference-Bold.eot) format('eot');
+      src: url(fonts/GlacialIndifference-Bold.woff2) format('woff2');
+      url(fonts/GlacialIndifference-Bold.woff) format('woff');
+      url(fonts/GlacialIndifference-Bold.eot) format('eot');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
   `;
