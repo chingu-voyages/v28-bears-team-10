448b96f5e6248169738c574e66935a06b54db81e
Jon Hill <jondhill1984@gmail.com>
2021-04-02 11:54:31 +0300

    Implement SWR as fetch method and place profiles in grid on page

--- pages/volunteers.js
+++ pages/volunteers.js
@@ -1,23 +1,38 @@
 import Head from 'next/head';
+import useSWR from 'swr';
 import styles from '../styles/Home.module.css';
 import { Box, SimpleGrid, Center, Text, Container } from '@chakra-ui/react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import VolnteerProfile from '../components/VolunteerProfile';
+import VolunteerProfile from '../components/VolunteerProfile';
+import { Grid } from '@chakra-ui/layout';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function VolunteersPage() {
+    const { data, error } = useSWR('http://localhost:3000/api/devs', fetcher);
+    console.log(data);
+    console.log(error);
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
     return (
         <>
-            <Layout>
-                <Container color="black" centerContent>
-                    <Text>This will be the Volunteers Page</Text>
-                    <Text mt="5px">
-                        Go back to{' '}
-                        <Link href="/" color="blue">
-                            home page
-                        </Link>
-                    </Text>
-                    <VolnteerProfile />
+            <Layout color="black">
+                <Container centerContent m={0}>
+                    <Grid
+                        placeSelf="center"
+                        pt={150}
+                        color="black"
+                        templateColumns="repeat(4, 1fr)"
+                        // templateRows="repeat(20, 1fr)"
+                        gap={5}>
+                        {
+                            // data &&
+                            data.map((user) => {
+                                return <VolunteerProfile key={user._id} user={user} />;
+                            })
+                        }
+                    </Grid>
                 </Container>
             </Layout>
         </>
