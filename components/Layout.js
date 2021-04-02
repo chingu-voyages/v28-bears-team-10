0521ca2d198d5e8150f56afada457b4dc1ec290d
Jon Hill <jondhill1984@gmail.com>
2021-04-02 11:55:47 +0300

    remove redundant css values from layout

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
                     content="Chinu.io project - an app that connects charities and volunteers to help with digital products andissues"
                 />
             </Head>
-            <Flex maxW="100%" h="100vh" my="0" mx="auto" justify="center" align="center">
+            <Flex maxW="100%" my="0" mx="auto" justify="center" align="center">
                 <Nav />
                 {children}
             </Flex>
