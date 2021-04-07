011b2a540c26e941b4988db85534e410c9bf0bdf
Armando Guarino <arm.guarino@gmail.com>
2021-04-07 18:24:20 +0200

    update signup page

--- components/Signup.js
+++ components/Signup.js
@@ -1,25 +1,10 @@
-import Head from 'next/head';
 import React, { useState } from 'react';
-import {
-    FormControl,
-    FormLabel,
-    Input,
-    FormHelperText,
-    Text,
-    Textarea,
-    Button,
-    Grid,
-    GridItem,
-    Select
-} from '@chakra-ui/react';
-import Link from 'next/link';
-import CountrySelector from './CountrySelector';
-import SkillsSelector from './SkillsSelector';
+import { Text, Select } from '@chakra-ui/react';
+import VolunteerForm from './VolunteerForm';
+import CharityForm from './CharityForm';
 
-import { useUser } from '@auth0/nextjs-auth0';
-
-export default function SignupForm() {
-    const { user, error, isLoading } = useUser();
+export default function Signup() {
+    const [userType, setUserType] = useState('unknown');
     const [form, setForm] = useState({
         username: '',
         email: 'New',
@@ -78,92 +63,16 @@ export default function SignupForm() {
         <>
             <form onSubmit={handleSubmit}>
                 <Text color="black">What type of profile would you like to set up?</Text>
-                <Select placeholder="User type" name="location" onChange={handleChange}>
+                <Select
+                    placeholder="User type"
+                    name="location"
+                    onChange={(e) => setUserType(e.target.value)}>
                     <option value="Volunteer">Volunteer</option>
                     <option value="Charity">Charity</option>
                 </Select>
-                <Grid templateColumns="1fr 1fr 1fr" columnGap={20} color="black">
-                    <GridItem mx={2}>
-                        <FormControl id="username" isRequired my={2}>
-                            <FormLabel>User name</FormLabel>
-                            <Input
-                                placeholder="Choose username "
-                                name="username"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                        <FormControl id="email" isRequired my={2}>
-                            <FormLabel>Email</FormLabel>
-                            <Input type="email" value={user.email} name="email" disabled />
-                        </FormControl>
-                        <FormControl id="country" my={2}>
-                            <FormLabel>Country</FormLabel>
-                            <CountrySelector onChange={handleChange} />
-                        </FormControl>
-                    </GridItem>
-                    <GridItem>
-                        <FormControl id="website" my={2}>
-                            <FormLabel>Website</FormLabel>
-                            <Input
-                                type="text"
-                                placeholder="Enter your website url"
-                                name="website"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                        <FormControl id="github" my={2}>
-                            <FormLabel>Github</FormLabel>
-                            <Input
-                                type="text"
-                                placeholder="Enter your Github"
-                                name="github"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                        <FormControl id="linkedin" my={2}>
-                            <FormLabel>Linkedin</FormLabel>
-                            <Input
-                                type="text"
-                                placeholder="Enter your linkedin"
-                                name="linkedin"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                        <FormControl id="twitter" my={2}>
-                            <FormLabel>Twitter</FormLabel>
-                            <Input
-                                type="text"
-                                placeholder="Enter your twitter handle"
-                                name="twitter"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                    </GridItem>
-                    <GridItem>
-                        <FormControl id="skills" my={2}>
-                            <FormLabel>Skills</FormLabel>
-                            <FormHelperText mb={1}>
-                                We recommend to pick up to 4 that you are most confident with
-                            </FormHelperText>
-                            <SkillsSelector onChange={handleChange} />
-                        </FormControl>
-                        <FormControl id="description" my={2}>
-                            <FormLabel>Description</FormLabel>
-                            <Textarea
-                                maxlength="150"
-                                placeholder="Write a sentance or two about yourself "
-                                name="description"
-                                onChange={handleChange}
-                            />
-                        </FormControl>
-                    </GridItem>
-                    <GridItem colSpan={2}>
-                        <Button mt={4} colorScheme="teal" type="submit">
-                            Submit
-                        </Button>
-                    </GridItem>
-                </Grid>
             </form>
+            {userType === 'Charity' && <CharityForm handleChange={handleChange} />}
+            {userType === 'Volunteer' && <VolunteerForm handleChange={handleChange} />}
         </>
     );
 }
