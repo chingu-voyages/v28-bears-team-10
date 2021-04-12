f5f2428cb323d5135ec090aabdef0abf99e4c602
Jon <56552404+jondhill333@users.noreply.github.com>
2021-04-12 11:37:50 +0300

    Add breakpoints for repsonsivity to theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,26 +1,35 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+const breakpoints = createBreakpoints({
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+});
 
 const theme = extendTheme({
-    styles: {
-        global: (props) => ({
-            body: {
-                overflowX: 'hidden',
-                color: props.colorMode === 'light' ? 'white' : 'black',
-                bg: props.colorMode === 'light' ? 'white' : 'black'
-            }
-        })
-    },
-    colors: {
-        transparent: 'transparent',
-        black: '#000',
-        white: '#fff',
-        orange: '#FF6700',
-        blue: '#268ED0'
-    },
-    fonts: {
-        body: 'Glacial Indifference Regular, sans-serif',
-        heading: 'Glacial Indifference Bold, sans-serif'
-    }
+  styles: {
+    global: (props) => ({
+      body: {
+        overflowX: "hidden",
+        color: props.colorMode === "light" ? "white" : "black",
+        bg: props.colorMode === "light" ? "white" : "black",
+      },
+    }),
+  },
+  colors: {
+    transparent: "transparent",
+    black: "#000",
+    white: "#fff",
+    orange: "#FF6700",
+    blue: "#268ED0",
+  },
+  fonts: {
+    body: "Glacial Indifference Regular, sans-serif",
+    heading: "Glacial Indifference Bold, sans-serif",
+  },
+  breakpoints,
 });
 
 export default theme;
