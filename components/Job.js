edac51dce4e1153976c0197b53d34d77bfbb1acd
Jon <56552404+jondhill333@users.noreply.github.com>
2021-04-12 17:48:53 +0300

    Add styling for job display component

--- components/Job.js
+++ components/Job.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
-import { Flex, Text, Grid } from "@chakra-ui/layout";
+import { Flex, Text, Grid, Heading, GridItem } from "@chakra-ui/layout";
 import { Tooltip, Image, Button, Avatar, Link } from "@chakra-ui/react";
 import { InfoOutlineIcon } from "@chakra-ui/icons";
-// import Link from 'next/link';
 import styles from "./Profile.module.css";
 
-export default function Profile({ job }) {
+export default function JobPosting({ job }) {
   const {
     sub,
     jobTitle,
@@ -15,129 +14,40 @@ export default function Profile({ job }) {
     jobHoursRequired,
     jobTechStack,
   } = job;
-  const [turned, setTurned] = React.useState(false);
-
-  const { flipcard, cardFront, cardBack, flip } = styles;
-
-  function handleClick() {
-    setTurned(!turned);
-  }
-  React.useEffect(() => {
-    setTurned(false);
-  }, []);
 
   return (
-    // @todo add job fields and style
     <>
       <Flex
         bg="blue"
         direction="column"
-        w="250px"
-        height="300px"
+        w="100%"
+        minHeight="400px"
         borderRadius="10px"
       >
         {!job && <Text color="black">Profile not found</Text>}
         <Flex
-          direction="column"
-          align="center"
-          w="100%"
-          height="37%"
-          borderBottom="orange 1px solid"
+          position="relative"
+          p={3}
+          flexDirection="column"
+          minHeight="400px"
+          height="auto"
         >
-          {/* <Avatar
-            border="solid 1px orange"
-            mt={1}
-            w="60px"
-            h="60px"
-            mx="auto"
-            src={avatar}
-          /> */}
-
-          <Text> {jobTitle}</Text>
+          <Heading fontSize="30px"> {jobTitle}</Heading>
           <Text> {jobDescription}</Text>
         </Flex>
-        <Flex
-          className={turned ? `${flip} ${flipcard}` : flipcard}
-          direction="column"
-          align="center"
-          justify="space-between"
-          py={2}
-          w="100%"
-          h="51%"
-        >
-          <Flex className={cardFront} direction="column" align="center">
-            {/* <Grid pt={0} w="50%" templateColumns="1fr 1fr">
-              {skills &&
-                skills.map((skill) => (
-                  <Text fontSize="15px" key={skill}>
-                    {skill}
-                  </Text>
-                ))}
-            </Grid> */}
-            {/* <Text fontSize="15px" px={2}>
-              {description && description.substr(0, 151)}
-            </Text> */}
-          </Flex>
-          <Flex
-            className={cardBack}
-            direction="column"
-            align="center"
-            justify="space-between"
-            p={0}
-            fontSize="14px"
-          >
-            {/* {website && (
-              <Link href={website} isExternal>
-                <Text>Website</Text>
-              </Link>
-            )}
-            {github && (
-              <Link href={github} isExternal>
-                <Text>Github</Text>
-              </Link>
-            )}
-            {linkedin && (
-              <Link href={linkedin} isExternal>
-                <Text>Linkedin</Text>
-              </Link>
-            )}
-            {twitter && (
-              <Link href={twitter} isExternal>
-                <Text>Twitter</Text>
-              </Link>
-            )}
-            {email && (
-              <Text transform="translateY(-5px)" py={2}>
-                {email || "available to connnect on social"}
-              </Text>
-            )} */}
-          </Flex>
-        </Flex>
-        <Flex justify="center" pt={2}>
-          <Button
-            onClick={handleClick}
-            w="35%"
-            h="20px"
-            bg="orange"
-            color="white"
-            borderRadius="10px"
-            fontSize="13px"
-          >
-            MORE INFO
-          </Button>
+        <Flex justifyContent="center" alignContent="center" py={2}>
           {job && (
-            <Button
-              w="35%"
-              h="20px"
-              bg="orange"
-              color="white"
-              borderRadius="10px"
-              fontSize="13px"
-            >
-              <Link href={`/profile/${sub}`}>VISIT PROFILE</Link>
+            <Button w="35%" bg="orange" color="white" borderRadius="10px">
+              <Link href={`/profile/${sub}`}>VISIT COMPANY PROFILE</Link>
             </Button>
           )}
         </Flex>
+        <Grid templateColumns={{ sm: "1fr", md: "1fr 1fr" }}>
+          <GridItem>{jobRequirements}</GridItem>
+          <GridItem>{jobExperienceLevel}</GridItem>
+          <GridItem>{jobHoursRequired}</GridItem>
+          <GridItem>{jobTechStack}</GridItem>
+        </Grid>
       </Flex>
     </>
   );
