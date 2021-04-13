507afe949922786cb59bb6be77c87b2223a56b99
Jon <56552404+jondhill333@users.noreply.github.com>
2021-04-13 12:09:17 +0300

    Style user card

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -23,6 +23,7 @@ import { useUser } from "@auth0/nextjs-auth0";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import UsersContext from "../../context/users/usersContext";
 import { useRouter } from "next/router";
+import { Grid, GridItem } from "@chakra-ui/layout";
 
 export default withPageAuthRequired(function profile() {
   const [loading, setLoading] = useState(false);
@@ -130,151 +131,202 @@ export default withPageAuthRequired(function profile() {
         </Container>
       </Layout>
     );
-  } else if (user && currentUser) {
+  }
+  if (user && currentUser) {
     return (
-      <Layout>
-        <Container maxW="container.lg">
-          {/* User card */}
-          {/* @todo style user card */}
-          <Box color="black" mt={20}>
-            <Image
-              w={200}
-              h={200}
-              src={currentUser.avatar}
-              alt={currentUser.username}
-            />
-            <Text>Your username is {currentUser.username}</Text>
-            <Text>Your email is {currentUser.email}</Text>
-            {currentUser.location && (
-              <Text>Your location is {currentUser.location}</Text>
-            )}
-            {currentUser.description && (
-              <Text>Your description is {currentUser.description}</Text>
-            )}
+      <>
+        <Layout>
+          <Container maxW="container.lg">
+            {/* <Flex> */}
+            <Flex direction="row" color="black" mt={20} mb={3}>
+              <Image
+                w={200}
+                h={200}
+                src={currentUser.avatar}
+                alt={currentUser.username}
+              />
+              <Grid
+                templateColumns="1fr 1fr"
+                // direction="column"
+                px={2}
+                // justifyContent="space-between"
+                py={1}
+              >
+                <GridItem>
+                  <Text mb={1}>
+                    <span style={{ fontWeight: "800" }}>Username:</span>{" "}
+                    {currentUser.username}
+                  </Text>
+                  <Text mb={1}>
+                    <span style={{ fontWeight: "800" }}>Email:</span>{" "}
+                    {currentUser.email}
+                  </Text>
+                  {currentUser.location && (
+                    <Text mb={1}>
+                      <span style={{ fontWeight: "800" }}>Location: </span>
+                      {currentUser.location}
+                    </Text>
+                  )}
+                  {currentUser.description && (
+                    <Text>
+                      <span style={{ fontWeight: "800" }}>Description:</span>{" "}
+                      <br />
+                      {currentUser.description}
+                    </Text>
+                  )}
+                </GridItem>
 
-            {/* {currentUser.skills && skills.map((skill) => <Text>{skill}</Text>)} */}
+                {/* {currentUser.skills && skills.map((skill) => <Text>{skill}</Text>)} */}
+                <GridItem>
+                  {currentUser.website && (
+                    <Text>
+                      {" "}
+                      <span style={{ fontWeight: "800" }}>Website: </span>
+                      {currentUser.website}
+                    </Text>
+                  )}
 
-            {currentUser.website && (
-              <Text>Your website is {currentUser.website}</Text>
-            )}
+                  {currentUser.twitter && (
+                    <Text>
+                      <span style={{ fontWeight: "800" }}>Twitter: </span>
+                      {currentUser.twitter}
+                    </Text>
+                  )}
 
-            {currentUser.twitter && (
-              <Text>Your twitter is {currentUser.twitter}</Text>
-            )}
+                  {currentUser.linkedin && (
+                    <Text>
+                      <span style={{ fontWeight: "800" }}>LinkedIn: </span>
+                      {currentUser.linkedin}
+                    </Text>
+                  )}
 
-            {currentUser.linkedin && (
-              <Text>Your linkedin is {currentUser.linkedin}</Text>
-            )}
-
-            {currentUser.github && (
-              <Text>Your github is {currentUser.github}</Text>
-            )}
-
+                  {currentUser.github && (
+                    <Text>
+                      <span style={{ fontWeight: "800" }}>Github: </span>
+                      {currentUser.github}
+                    </Text>
+                  )}
+                </GridItem>
+              </Grid>
+            </Flex>
             {/* Update  */}
-            {currentUser.sub === user.sub && (
-              <Link href="/profile/update">
-                <Button colorScheme="teal">Update Profile</Button>
-              </Link>
-            )}
-
-            {/* Delete */}
-            {currentUser.sub === user.sub && (
-              <>
-                <Button colorScheme="red" onClick={() => setIsOpen(true)}>
-                  Delete Profile
-                </Button>
-
-                <AlertDialog
-                  isOpen={isOpen}
-                  leastDestructiveRef={cancelRef}
-                  onClose={onClose}
-                >
-                  <AlertDialogOverlay color="black">
-                    <AlertDialogContent>
-                      <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                        Delete
-                      </AlertDialogHeader>
-
-                      <AlertDialogBody>
-                        Are you sure? You can't undo this action afterwards.
-                      </AlertDialogBody>
-
-                      <AlertDialogFooter>
-                        <Button ref={cancelRef} onClick={onClose}>
-                          Cancel
-                        </Button>
-                        <Button
-                          colorScheme="red"
-                          onClick={() => onDelete()}
-                          ml={3}
-                        >
-                          Delete
-                        </Button>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialogOverlay>
-                </AlertDialog>
-              </>
-            )}
-
-            {currentUser.sub === user.sub &&
-              currentUser.userType === "charity" && (
-                <Link href={`/${router.query.id}/addJob`}>
-                  <Button colorScheme="cyan">Add Job</Button>
+            <Flex
+              w="40%"
+              justifyContent="space-between"
+              placeSelf="center"
+              mb={3}
+            >
+              {currentUser.sub === user.sub && (
+                <Link href="/profile/update">
+                  <Button colorScheme="teal">Update Profile</Button>
                 </Link>
               )}
-          </Box>
 
-          {/* Jobs */}
-          {currentUser.userType === "charity" && jobs && jobs.length > 0 && (
-            <>
-              {jobs.filter((job) => job.postedBy === currentUser.email)
-                .length !== 0 && (
-                <Heading size="xl" color="black">
-                  Jobs Posted:
-                </Heading>
+              {/* Delete */}
+              {currentUser.sub === user.sub && (
+                <>
+                  <Button colorScheme="red" onClick={() => setIsOpen(true)}>
+                    Delete Profile
+                  </Button>
+
+                  <AlertDialog
+                    isOpen={isOpen}
+                    leastDestructiveRef={cancelRef}
+                    onClose={onClose}
+                  >
+                    <AlertDialogOverlay color="black">
+                      <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                          Delete
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                          Are you sure? You can't undo this action afterwards.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                          <Button ref={cancelRef} onClick={onClose}>
+                            Cancel
+                          </Button>
+                          <Button
+                            colorScheme="red"
+                            onClick={() => onDelete()}
+                            ml={3}
+                          >
+                            Delete
+                          </Button>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialogOverlay>
+                  </AlertDialog>
+                </>
               )}
 
-              <Box color="black">
-                {jobs
-                  .filter((job) => job.postedBy === currentUser.email)
-                  .map((job, index) => (
-                    /* @todo style jobs boxes */
-                    <Box key={index} border="2px solid red">
-                      <Text>{job.jobTitle}</Text>
-                      <Text>{job.jobDescription}</Text>
+              {currentUser.sub === user.sub &&
+                currentUser.userType === "charity" && (
+                  <Link href={`/${router.query.id}/addJob`}>
+                    <Button colorScheme="cyan">Add Job</Button>
+                  </Link>
+                )}
+            </Flex>
 
-                      {job.postedBy === user.email && (
-                        <Button
-                          onClick={() => deleteJob(job._id)}
-                          colorScheme="red"
-                        >
-                          Delete
-                        </Button>
-                      )}
-                    </Box>
-                  ))}
-              </Box>
-            </>
-          )}
-        </Container>
-      </Layout>
+            {/* Jobs */}
+            {currentUser.userType === "charity" && jobs && jobs.length > 0 && (
+              <>
+                {jobs.filter((job) => job.postedBy === currentUser.email)
+                  .length !== 0 && (
+                  <Heading size="xl" color="black" mb={1}>
+                    Posted Jobs:
+                  </Heading>
+                )}
+
+                <Box color="black">
+                  {jobs
+                    .filter((job) => job.postedBy === currentUser.email)
+                    .map((job, index) => (
+                      /* @todo style jobs boxes */
+                      <Box key={index} border="2px solid orange" mb={2} p={2}>
+                        <Heading mb={2} fontSize="20px">
+                          {job.jobTitle}
+                        </Heading>
+                        <Text>{job.jobDescription}</Text>
+
+                        {job.postedBy === user.email && (
+                          <Button
+                            onClick={() => deleteJob(job._id)}
+                            colorScheme="red"
+                            mt={2}
+                          >
+                            Delete
+                          </Button>
+                        )}
+                      </Box>
+                    ))}
+                </Box>
+              </>
+            )}
+          </Container>
+        </Layout>
+      </>
     );
-  } else if (user) {
+  }
+  if (user) {
     return (
-      <Layout>
-        <Container maxW="container.lg">
-          <Box color="black" mt={20}>
-            <Image w={200} h={200} src={user.picture} alt={user.nickname} />
-            <Text>Your nickname is {user.nickname}</Text>
-            <Text>Your email is {user.email}</Text>
+      <>
+        <Layout>
+          <Container maxW="container.lg">
+            <Box color="black" mt={20}>
+              <Image w={200} h={200} src={user.picture} alt={user.nickname} />
+              <Text>Your nickname is {user.nickname}</Text>
+              <Text>Your email is {user.email}</Text>
 
-            <Button colorScheme="teal">
-              <Link href="/profile/signup">Create your profile</Link>
-            </Button>
-          </Box>
-        </Container>
-      </Layout>
+              <Button colorScheme="teal">
+                <Link href="/profile/signup">Create your profile</Link>
+              </Button>
+            </Box>
+          </Container>
+        </Layout>
+      </>
     );
   } else {
     return <h1>Loading...</h1>;
